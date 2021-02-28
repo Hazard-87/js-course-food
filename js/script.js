10f9ee040ef4b9a59a3bc674a589f8f5ccbf3b46
Hazard <dpetryshkin@gmail.com>
2021-03-01 02:53:43 +0300

    Подключили axios

--- js/script.js
+++ js/script.js
@@ -184,47 +184,56 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  fetch('http://localhost:3000/menu')
-    .then((data) => data.json())
-    .then((res) =>
-      res.map((item) =>
-        new MenuCard(
-          item.img,
-          item.altimg,
-          item.title,
-          item.descr,
-          item.price,
-          '.menu .container',
-        ).render(),
-      ),
+  axios.get('http://localhost:3000/menu').then(({ data }) => {
+    data.forEach(({ img, altimg, title, descr, price }) =>
+      new MenuCard(img, altimg, title, descr, price, '.menu .container').render(),
     );
+  });
 
-  // new MenuCard(
-  //   'img/tabs/vegy.jpg',
-  //   'vegy',
-  //   'Меню "Фитнес"',
-  //   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-  //   9,
-  //   '.menu .container',
-  // ).render();
+  // const getResource = async (url) => {
+  //   const res = await fetch(url);
 
-  // new MenuCard(
-  //   'img/tabs/elite.jpg',
-  //   'elite',
-  //   'Меню “Премиум”',
-  //   'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-  //   18,
-  //   '.menu .container',
-  // ).render();
+  //   if (!res.ok) {
+  //     new Error(`Could not fetch ${url}, status ${res.status}`);
+  //   }
+  //   return await res.json();
+  // };
 
-  // new MenuCard(
-  //   'img/tabs/post.jpg',
-  //   'post',
-  //   'Меню "Постное"',
-  //   'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-  //   13,
-  //   '.menu .container',
-  // ).render();
+  // getResource('http://localhost:3000/menu').then((data) =>
+  //   data.forEach(({ img, altimg, title, descr, price }) =>
+  //     new MenuCard(img, altimg, title, descr, price, '.menu .container').render(),
+  //   ),
+  // );
+
+  // getResource('http://localhost:3000/menu').then((data) => {
+  //   createCard(data);
+  // });
+
+  // function createCard(data) {
+  //   data.forEach(({ img, altimg, title, descr, price }) => {
+  //     const element = document.createElement('div');
+  //     const parent = document.querySelector('.menu .container');
+
+  //     price = price * 75;
+
+  //     element.classList.add('menu__item');
+
+  //     element.innerHTML = `
+  //         <img src=${img} alt=${altimg} />
+  //         <h3 class="menu__item-subtitle">${title}</h3>
+  //         <div class="menu__item-descr">${descr}</div>
+  //         <div class="menu__item-divider"></div>
+  //         <div class="menu__item-price">
+  //           <div class="menu__item-cost">Цена:</div>
+  //           <div class="menu__item-total">
+  //             <span>${price}</span> руб/день
+  //           </div>
+  //         </div>
+  //       `;
+
+  //     parent.append(element);
+  //   });
+  // }
 
   // Forms
 
@@ -267,12 +276,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
       const formData = new FormData(form);
 
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      postData('http://localhost:3000/requests', JSON.stringify(object))
+      postData('http://localhost:3000/requests', json)
         .then((data) => {
           console.log(data);
           showThanksModal(message.success);
@@ -322,15 +328,4 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     });
   }
-
-  // fetch('db.json')
-  //   .then((data) => data.json())
-  //   .then(({ menu }) => {
-  //     menu.map(
-  //       (item, index) => (
-  //         (descr[index].innerHTML = `${item.descr}`), (title[index].innerHTML = `${item.title}`),
-  //         ()
-  //       ),
-  //     );
-  //   });
 });
